9de264cbc96d80e469efb434401599a9c9b99283
Aaron Turner <aaron@aaronthedev.com>
2017-04-22 19:13:24 -0700

    Made the client more 'Reusable'

--- src/app/components/page/page.js
+++ src/app/components/page/page.js
@@ -1,8 +1,9 @@
 class PageController {
-  constructor($log, $timeout, $sce, $stateParams, sayonaraService) {
+  constructor($log, $timeout, $sce, $stateParams, $location, sayonaraService) {
     this.$log = $log;
     this.$timeout = $timeout;
     this.$sce = $sce;
+    this.$location = $location;
     this.$stateParams = $stateParams;
     this.sayonaraService = sayonaraService;
 
@@ -26,22 +27,39 @@ class PageController {
     }
 
     // Check the cache before iterating
-    if (this.$stateParams.title === this.currentPage.title) {
+    if (this.$stateParams.title &&
+      this.$stateParams.title === this.currentPage.title) {
       return this.currentPage;
     }
 
     // Find the page by its title
+    // Default page is Home
     let foundPage = false;
     this.sayonaraSite.pages.some(page => {
-      if (page.title === this.$stateParams.title) {
+      if (this.$stateParams.title &&
+        page.title === this.$stateParams.title) {
         foundPage = page;
         return true;
       }
       return false;
     });
-    // cache the result
-    this.currentPage = foundPage;
-    return foundPage;
+
+    // If we did not find a result
+    if (foundPage) {
+      // cache the result
+      this.currentPage = foundPage;
+    } else {
+      // Simply return the First (Default) page
+      this.currentPage = this.sayonaraSite.pages[0];
+    }
+    return this.currentPage;
+  }
+
+  /**
+   * Ng-click to navigate to entry
+   */
+  goToEntry(entry) {
+    this.$location.path('/entry/' + entry.title);
   }
 }
 
